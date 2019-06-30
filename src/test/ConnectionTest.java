88e1e57a6fbe33220b2fed973585074cf2009181
fidelity1827@gmail.com <fidelity1827@gmail.com>
2019-06-30 12:19:06 -0400

    new line added

--- src/test/ConnectionTest.java
+++ src/test/ConnectionTest.java
@@ -4,5 +4,7 @@ public class ConnectionTest {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 		System.out.println("Got connected!");
+		System.out.println("Got connected!");
+		
 	}
 }
